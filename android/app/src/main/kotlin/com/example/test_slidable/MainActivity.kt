acf4ca694de1e9405547c6b13d8b5f95356de5ab
youcef <salmywsf964@gmail.com>
2025-02-23 14:08:09 +0100

    creation of project

---
+++ android/app/src/main/kotlin/com/example/test_slidable/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_slidable
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+     );
+  }
+}
+
+
+
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+
